cad4423d8957b4b25aecab299e3abed2ef40cb62
Pooja-Dhuri <dhuripooja01@gmail.com>
2024-03-11 17:53:24 +0530

    working on js part

---
+++ script.js
@@ -0,0 +1,122 @@
+const canvas = document.querySelector("canvas"),
+  toolBtns = document.querySelectorAll(".tool"),
+  fillColor = document.querySelector("#fill-color"),
+  sizeSlider = document.querySelector("#size-slider"),
+  colorBtns = document.querySelectorAll(".colors .option"),
+  colorPicker = document.querySelector("#color-picker"),
+  clearCanvas = document.querySelector(".clear-canvas"),
+  saveImage = document.querySelector(".save-img"),
+  ctx = canvas.getContext("2d");
+
+let prevMouseX,
+  prevMouseY,
+  snapShot,
+  isDrawing = false,
+selectedTool = "brush", 
+brushWidth = 5,
+selectedColor="#000"
+
+window.addEventListener("load", () => {
+  //setting canvas width/height.. offsetwidth/height returns viewable width/height of an element
+  canvas.width = canvas.offsetWidth;
+  canvas.height = canvas.offsetHeight;
+});
+
+const drawRect = (e) => {
+  if (!fillColor.checked) {
+    return ctx.strokeRect(
+      e.offsetX,
+      e.offsetY,
+      prevMouseX - e.offsetX,
+      prevMouseY - e.offsetY
+    );
+  }
+  ctx.fillRect(
+    e.offsetX,
+    e.offsetY,
+    prevMouseX - e.offsetX,
+    prevMouseY - e.offsetY
+  );
+};
+
+const drawCircle = (e) => {
+  ctx.beginPath();
+  let radius = Math.sqrt(
+    Math.pow(prevMouseX - e.offsetX, 2) + Math.pow(prevMouseY - e.offsetY, 2)
+  );
+  ctx.arc(prevMouseX, prevMouseY, radius, 0, 2 * Math.PI);
+  fillColor.checked ? ctx.fill() : ctx.stroke();
+};
+
+const drawTriangle = (e) => {
+  ctx.beginPath(); //creating a new path to draw circle
+  ctx.moveTo(prevMouseX, prevMouseY); //moving triangle to the mouse pointer
+  ctx.lineTo(e.offsetX, e.offsetY); //creating first lie according to the mouse pointer
+  ctx.lineTo(prevMouseX * 2 - e.offsetX, e.offsetY); //creting bottom line of triangle
+  ctx.closePath(); //closing path the triangle so the third line draw automatically
+  fillColor.checked ? ctx.fill() : ctx.stroke(); //if fillcolor is checked fill triangle else draw border
+};
+
+const startDraw = (e) => {
+  isDrawing = true;
+  prevMouseX = e.offsetX;
+  prevMouseY = e.offsetY;
+  ctx.beginPath(); //creating new path to draw
+  ctx.lineWidth = brushWidth; //passing brush size as a width
+  ctx.strokeStyle=selectedColor;
+  ctx.fillStyle=selectedColor;
+  snapShot = ctx.getImageData(0, 0, canvas.width, canvas.height);
+};
+
+const drawing = (e) => {
+  if (!isDrawing) return;
+  ctx.putImageData(snapShot, 0, 0);
+
+  if (selectedTool === "brush" || selectedTool === "eraser" ) {
+    ctx.strokeStyle=selectedTool==="eraser" ? "#fff" : selectedColor;
+    ctx.lineTo(e.offsetX, e.offsetY); //creating line according to the mouse pointer
+    ctx.stroke(); //drawing/filling line with color
+  } else if (selectedTool === "rectangle") {
+    drawRect(e);
+  } else if (selectedTool === "circle") {
+    drawCircle(e);
+  } else {
+    drawTriangle(e);
+  }
+};
+
+toolBtns.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    document.querySelector(".options .active").classList.remove("active");
+    btn.classList.add("active");
+    selectedTool = btn.id;
+    console.log(selectedTool);
+  });
+});
+
+sizeSlider.addEventListener("change", () => (brushWidth = sizeSlider.value)); //passing slider value as brushSize
+
+colorBtns.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    document.querySelector(".options .selected").classList.remove("selected");
+    btn.classList.add("selected");
+    selectedColor=window.getComputedStyle(btn).getPropertyValue("background-color");
+  });
+});
+
+colorPicker.addEventListener("change",()=>{
+    colorPicker.parentElement.style.background=colorPicker.value;
+    colorPicker.parentElement.click();
+})
+
+clearCanvas.addEventListener("click",()=>{
+    ctx.clearRect(0,0,canvas.width,canvas.height)
+})
+
+saveImage.addEventListener("click",)
+
+canvas.addEventListener("mousedown", startDraw);
+canvas.addEventListener("mousemove", drawing);
+canvas.addEventListener("mouseup", () => {
+  isDrawing = false;
+});
